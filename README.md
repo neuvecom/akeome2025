e3b03e00372ef51ccd5a3b26fa33434fe6495752
ss-yoshiharu-sato <yoshiharu.sato@supership.jp>
2024-12-29 19:02:57 +0900

    ちょっと改良

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # akeome2025
-for New Year's Greetings
+for New Year's Greetings.
+
+## メモ
+- エディタを開く `uv run pyxel edit`  
+- 起動する `uv run pyxel run akeome.py`
+- pyxapp化 uv run pyxel package . akeome.py
+- html出力 uv run pyxel app2html akeome2025.pyxapp
--- akeome.py
+++ akeome.py
@@ -1,4 +1,5 @@
 import pyxel
+import random
 
 pyxel.init(128,64)
 pyxel.load("my_resource.pyxres")
@@ -6,10 +7,17 @@ pyxel.load("my_resource.pyxres")
 x = 0
 y = 0
 status = 0
+tutu_status = 0
+
 pyxel.playm(1, loop=True)
 
+tutu_x = random.randint(1, 112)
+tutu_y = random.randint(1, 48)
+
+print('BINGO is ' + str(tutu_x) + ':' + str(tutu_y))
+
 def update():
-    global x,y,status
+    global x,y,status,tutu_x,tutu_y,tutu_status
 
     # 座標の修正
     if pyxel.mouse_x < 0:
@@ -30,7 +38,12 @@ def update():
         status = 1
     else:
         status = 0
-        
+
+    if x == tutu_x and y == tutu_y:
+        tutu_status = 1
+    else:
+        tutu_status = 0
+
     return
 
 def draw():
@@ -40,12 +53,16 @@ def draw():
     pyxel.text(11, 24, "|", 7)
     pyxel.text(6, 32, "LET'S MOVE CAT HERE!", 7)
     pyxel.text(84, 55, "2025.01.01", 8)
+    pyxel.text(1, 55, str(x) + ':' + str(y) +  ':' + str(tutu_x) + ':' + str(tutu_y) + ':' + str(tutu_status), 7)
 
     pyxel.blt(x, y, 0, 0, 0, 16, 16)
 
     if status == 1:
         # pyxel.play(0, 38)
         pyxel.text(30, 12, "A HAPPY NEW YEAR!", 9)
+
+    if tutu_status == 1:
+        pyxel.text(30, 12, "BINGO!", 9)
 
     return
 
